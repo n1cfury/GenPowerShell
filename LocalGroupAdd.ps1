7c9176909d500baffbe8c291c87b4cc5e29dd36f
n1cfury <fukugetin@gmail.com>
2017-02-02 19:53:07 -0800

    Updated Description for Local Group Add powershell script

---
+++ LocalGroupAdd.ps1
@@ -0,0 +1,11 @@
+﻿#This is a sysadmin tool used for adding multiple domain user objects to local groups
+
+#This could be tweaked to add to an AD group 
+
+$me = [environment]::Username
+write-host "Enter the name of the group you want to add"
+write-host "from $users"
+$groupname = read-host "User names from $users"
+$users = get-content -path "C:\users\$me\desktop\users.txt"
+foreach ($u in $users)
+	{net localgroup $groupname /add $u}
