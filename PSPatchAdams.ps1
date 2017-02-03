2dd20cb7e614df9ba7a78aa6414cdf2196bb966b
n1cfury <fukugetin@gmail.com>
2017-02-03 09:19:22 -0800

    Removing old gneeric .ps1 files

--- PSPatchAdams.ps1
+++
@@ -1,5 +0,0 @@
-﻿#Powershell Playset
-#confirm KB article you enter is installed on the server(s) you're checking
-
-$patch = read-host "Enter KB article for the patch"
-get-hotfix -id $patch |Out-GridView
--- PSServertest.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Powershell Playset
-#
-
-[environment]::Username
-$Computers = Get-Content "c:\users\conkywadmin\desktop\servers.txt"
-Test-Connection $Computers | Out-File "c:\users\conkywadmin\desktop\servers.txt"
